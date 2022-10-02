7850bd0fec6da6dea352e6a221aa93833012788c
Fergan Yalim <ferganyalim13@gmail.com>
2022-10-02 01:12:56 +0100

    Complete the function

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
+import collections
+
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
+    for i, item in enumerate(items):
+        items[i] = str(item)
+    frequencies = dict(collections.Counter(items))
     # Your code goes here
     return frequencies
